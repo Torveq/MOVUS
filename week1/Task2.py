ff3d7f87cef116606dd1408d732d3e6a79415346
Mahmoud Mohammad Alhasan <mahmoud.mohammadalhasan@student.manchester.ac.uk>
2024-09-30 17:01:48 +0100

    Second task

---
+++ week1/Task2.py
@@ -0,0 +1,9 @@
+def printState():
+    print("Red Light is " + str(redLight))
+    print("Yellow Light is " + str(yellowLight))
+    print("Green Light is " + str(greenLight))
+
+redLight = 1
+yellowLight = 0
+greenLight = 0
+printState()
